38210a0a813386d965f8182ef6c47c990257e20a
nikolakulikova <kulikova.n12345@gmail.com>
2023-01-13 18:08:19 +0100

    added baseline

--- classification.py
+++ classification.py
@@ -1,18 +1,21 @@
 from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
-from sklearn.model_selection import LearningCurveDisplay, ShuffleSplit
+from sklearn.model_selection import LearningCurveDisplay
 from sklearn.neighbors import KNeighborsClassifier
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from collections import Counter
 
 
 def final_predict(X_train, y_train, X_test, y_test):
     models = [{"model": RandomForestClassifier(n_estimators=64, max_depth=7), "name": "RFC"},
               {"model": svm.SVC(kernel='rbf', random_state=0, C=0.2, gamma=0.16), "name": "SVC"},
-              {"model": KNeighborsClassifier(n_neighbors=9), "name": "KNC"}]
+              {"model": KNeighborsClassifier(n_neighbors=67), "name": "KNC"}]
 
+    print("Baseline for train: {}".format(baseline(y_train) / len(y_train)))
+    print("Baseline for test: {}".format(baseline(y_test) /  len(y_test)))
     for model in models:
         model.get("model").fit(X_train, y_train)
         print(model.get("name"))
@@ -30,6 +33,7 @@ def final_predict(X_train, y_train, X_test, y_test):
         LearningCurveDisplay.from_estimator(model.get("model"), **common_params)
         plt.show()
 
+
 def mlp(X, y):
     mlp = Sequential()
     mlp.add(Dense(16, activation='sigmoid', input_dim=X.shape[1]))
@@ -41,6 +45,7 @@ def mlp(X, y):
 
     history = mlp.fit(X, keras.utils.to_categorical(y), epochs=100, validation_split=0.1, verbose=False)
 
+    # print(history.history["accuracy"][-1])
     plt.figure()
     plt.plot(history.history['loss'], label='training loss')
     plt.plot(history.history['val_loss'], label='validation loss')
@@ -86,7 +91,7 @@ def get_hyper_param_SVC(X_train, y_train, X_test, y_test):
 
 def get_hyper_param_KNC(X_train, y_train, X_test, y_test):
     best_n, best_test = 0, 0
-    for n in range(1, 11):
+    for n in range(1, 70):
         model = KNeighborsClassifier(n_neighbors=n)
         model.fit(X_train, y_train)
         score = model.score(X_test, y_test)
@@ -94,3 +99,7 @@ def get_hyper_param_KNC(X_train, y_train, X_test, y_test):
             best_n = n
             best_test = score
     return best_n
+
+
+def baseline(y):
+    return Counter(y)[5]
